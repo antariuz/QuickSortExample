b44cde7efee8c0978726efb364ea868ec48a9ba7
Tymur Foshch <antariuz@gmail.com>
2021-08-24 20:36:00 +0300

    done

--- src/QuickSortExample.java
+++ src/QuickSortExample.java
@@ -1,5 +1,40 @@
+import java.util.Arrays;
+
 public class QuickSortExample {
-    public static void main(String[] args) {
+
+    public static int initialSort(int[] array, int firstIndex, int lastIndex) {
+        int pivot = array[lastIndex];
+        int i = firstIndex - 1;
+        for (int j = firstIndex; j <= lastIndex - 1; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, lastIndex);
+        return i + 1;
+    }
+
+    public static void quickSort(int[] array, int firstIndex, int lastIndex) {
+        if (firstIndex < lastIndex) {
+            int point = initialSort(array, firstIndex, lastIndex);
+            quickSort(array, firstIndex, point - 1);
+            quickSort(array, point + 1, lastIndex);
+        }
 
     }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {10, 80, 30, 90, 40, 50, 70};
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
 }
